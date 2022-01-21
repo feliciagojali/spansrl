2b666e5fb87def6b923842e665e38fe088d0192c
Felicia Gojali <gojalif@gmail.com>
2022-01-21 17:09:52 +0700

    feat: add sh command

---
+++ src/data/fetch_all_datas.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+DATA_PATH="./data"
+if [ ! -d $DATA_PATH ]; then
+  mkdir -p $DATA_PATH
+fi
+
+cd $DATA_PATH
+
+RAW_PATH="./raw"
+
+if [ ! -d $RAW_PATH ]; then
+  mkdir -p $RAW_PATH
+fi
+
+cd $RAW_PATH
+
+echo "Downloading pretrained models"
+wget "https://drive.google.com/file/d/1vL8vyfJbaj3i91peTu738jq25N-yhsU3/view?usp=sharing"
