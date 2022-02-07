649745099242888ed8c7735c448dc70e46242314
Felicia Gojali <gojalif@gmail.com>
2022-02-07 11:59:16 +0700

    Update SRLData.py

--- src/features/SRLData.py
+++ src/features/SRLData.py
@@ -7,7 +7,7 @@ from collections import Counter
 currentdir = os.path.dirname(os.path.realpath(__file__))
 parentdir = os.path.dirname(currentdir)
 sys.path.append(parentdir)
-from features.helper import save_npy, _print_f1, check_pred_id, split_first, label_encode, get_span_idx, pad_input, extract_bert, extract_pas_index, save_emb, convert_idx
+from helper import save_npy, _print_f1, check_pred_id, split_first, label_encode, get_span_idx, pad_input, extract_bert, extract_pas_index, save_emb, convert_idx
 from utils.utils import create_span
 import numpy as np
 import tensorflow as tf
