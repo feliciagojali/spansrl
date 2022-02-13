8b08cc8cc4e6237a2b96722bc86f0340a6bbae55
Felicia Gojali <gojalif@gmail.com>
2022-02-13 23:26:04 +0700

    Update train.py

--- src/train.py
+++ src/train.py
@@ -45,15 +45,15 @@ def main():
     out = np.load(dir + config['output'], mmap_mode='r')
 
     # Features loading
-    # dir = config['features_dir'] +'val_'
-    # if (not config['use_fasttext']):
-    #     features_1 = np.load(dir +config['features_1'], mmap_mode='r')
-    # else :
-    #     features_1 = np.load(dir +config['features_1.1'], mmap_mode='r')
-    # features_2 = np.load(dir+config['features_2'], mmap_mode='r')
-    # features_3 = np.load(dir+config['features_3'], mmap_mode='r')
-    # input_val = [features_1, features_2, features_3]
-    # out_val = np.load(dir + config['output'], mmap_mode='r')
+    dir = config['features_dir'] +'val_'
+    if (not config['use_fasttext']):
+        features_1 = np.load(dir +config['features_1'], mmap_mode='r')
+    else :
+        features_1 = np.load(dir +config['features_1.1'], mmap_mode='r')
+    features_2 = np.load(dir+config['features_2'], mmap_mode='r')
+    features_3 = np.load(dir+config['features_3'], mmap_mode='r')
+    input_val = [features_1, features_2, features_3]
+    out_val = np.load(dir + config['output'], mmap_mode='r')
 
     # Training Parameters
     batch_size = config['batch_size']
@@ -70,7 +70,7 @@ def main():
         lr_schedule = initial_learning_rate
     # Compiling, fitting and saving model
     model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=lr_schedule), loss=tf.keras.losses.CategoricalCrossentropy())
-    model.fit(input, out, batch_size=batch_size, epochs=epochs, callbacks=[callback])
+    model.fit(input, out, batch_size=batch_size, validation_data=[input_val, out_val], epochs=epochs, callbacks=[callback])
     model.save('models/'+ config['model_path'])
 
 
