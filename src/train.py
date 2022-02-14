c16a3f13b71f2e1f826b1654a0c8176d1a412298
Felicia Gojali <gojalif@gmail.com>
2022-02-14 11:54:03 +0700

    Update train.py

--- src/train.py
+++ src/train.py
@@ -54,6 +54,7 @@ def main():
             staircase=True)
     else:
         lr_schedule = initial_learning_rate
+    model = SRL(config)
     # Compiling, fitting and saving model
     model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=lr_schedule), loss=tf.keras.losses.CategoricalCrossentropy())
     model.fit(input, out, batch_size=batch_size, validation_data=(input_val, out_val), epochs=epochs, callbacks=[callback])
